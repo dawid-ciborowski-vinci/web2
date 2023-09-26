c95f790f79b3beddaa4548461b4d04d6e443d76c
dawid-ciborowski-vinci <dawid.ciborowski@student.vinci.be>
2023-09-26 12:44:11 +0200

    1.3 GET filtre + get id

--- exercices/1.3/myMovies/routes/films.js
+++ exercices/1.3/myMovies/routes/films.js
@@ -25,9 +25,36 @@ let FILMS = [
     },
 ];
 
-router.get('/', function(req, res, next) {
+/*router.get('/', function(req, res, next) {
     console.log("GET /films");
     res.json(FILMS);
-  });
+});*/
+
+router.get('/', function(req, res, next) {
+    let min =
+        req?.query['minimum-duration']
+        ? req.query['minimum-duration']
+        : undefined;
+    console.log("Min : " + min);
+    let orderedMovies = [];
+    
+    if (isNaN(min)) {
+        orderedMovies.push("Put a number. Bitch")
+    } else {
+        min = parseInt(min)
+        FILMS.forEach(element => {
+        if (element.duration >= min) {
+            orderedMovies.push(element);
+        }
+    });
+    }
+    res.json(orderedMovies)
+});
+
+router.get('/:id', (req, res, next) => {
+    const indexOfMovieFound = FILMS.findIndex((movie) => movie.id == req.params.id);
+    if (indexOfMovieFound < 0) return res.sendStatus(404);
+    res.json(FILMS[indexOfMovieFound]);
+});
 
 module.exports = router;
