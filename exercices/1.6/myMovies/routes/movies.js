b5f3a667dd833bb874fc2fef94befc6ca44d73ef
dawid-ciborowski-vinci <dawid.ciborowski@student.vinci.be>
2023-09-29 15:44:06 +0200

    1.6 : API : delete & modify

---
+++ exercices/1.6/myMovies/routes/movies.js
@@ -0,0 +1,164 @@
+var express = require('express');
+var router = express.Router();
+
+let MOVIES = [
+    {
+        id: 1,
+        title: "American Pie",
+        duration: 90,
+        budget: 200000,
+        link: "imdb"
+    },
+    {
+        id: 2,
+        title: "Hunger Games",
+        duration: 120,
+        budget: 1000000,
+        link: "imdb"
+    },
+    {
+        id: 3,
+        title: "Jaws",
+        duration: 93,
+        budget: 300000,
+        link: "imdb"
+    },
+];
+
+router.get('/', (req, res, next) => {
+    console.log("GET ALL");
+    res.json(MOVIES);
+});
+
+router.get('/', function(req, res, next) {
+    let min =
+        req?.query['minimum-duration']
+        ? req.query['minimum-duration']
+        : undefined;
+    console.log("Min : " + min);
+    let orderedMovies = [];
+    
+    if (isNaN(min)) {
+        return res.sendStatus(400);
+    } else {
+        min = parseInt(min)
+        MOVIES.forEach(element => {
+        if (element.duration >= min) {
+            orderedMovies.push(element);
+        }
+    });
+    }
+    res.json(orderedMovies)
+});
+
+router.get('/:id', (req, res, next) => {
+    const indexOfMovieFound = MOVIES.findIndex((movie) => movie.id == req.params.id);
+    if (indexOfMovieFound < 0) return res.sendStatus(404);
+    res.json(MOVIES[indexOfMovieFound]);
+});
+
+router.post('/', (req, res) => {
+    const title = req?.body?.title?.trim().length !== 0 ? req.body.title : undefined;
+    const duration = !isNaN(req?.body?.duration) && req?.body?.duration > 0 ? req.body.duration : undefined;
+    const budget = !isNaN(req?.body?.budget) && req?.body?.budget > 0 ? req.body.budget : undefined;
+    const link = req?.body?.link?.trim().length !== 0 ? req.body.link : undefined;
+
+    if (!title || !link || !duration || !budget) return res.sendStatus(400);
+
+    let foundMovie = MOVIES.find(function(movie) {
+        return movie.title === title;
+    });
+
+    if (foundMovie) return res.sendStatus(409);
+
+    const nextId = MOVIES.length + 1 
+
+    const newMovie = { id: nextId, title, link, duration, budget };
+
+    MOVIES.push(newMovie);
+
+    return res.json(newMovie);
+});
+
+router.delete('/:id', (req, res) => {
+    const id = req?.params?.id ? req.params.id : undefined;
+    if (isNaN(id)) return res.sendStatus(400);
+
+    let foundIndex = MOVIES.findIndex(movie => movie.id == req.params.id);
+
+    if (foundIndex < 0) return res.sendStatus(404);
+
+    const removedItems = MOVIES.splice(foundIndex, 1);
+    const itemRemoved = removedItems[0];
+
+    res.json(itemRemoved);
+});
+
+router.patch('/:id', (req, res) => {
+    console.log(`PATCH /movies/${req.params.id}`);
+
+    const title = req?.body?.title?.trim().length !== 0 ? req.body.title : undefined;
+    const duration = !isNaN(req?.body?.duration) && req?.body?.duration > 0 ? req.body.duration : undefined;
+    const budget = !isNaN(req?.body?.budget) && req?.body?.budget > 0 ? req.body.budget : undefined;
+    const link = req?.body?.link?.trim().length !== 0 ? req.body.link : undefined;
+    
+  
+    console.log('POST /movies');
+  
+    if (!title && !duration && !budget && !link) return res.sendStatus(400);
+  
+    const foundIndex = MOVIES.findIndex(movie => movie.id == req.params.id);
+  
+    if (foundIndex < 0) return res.sendStatus(404);
+  
+    const updatedMovie = {...MOVIES[foundIndex], ...req.body};
+  
+    MOVIES[foundIndex] = updatedMovie;
+  
+    res.json(updatedMovie);
+});
+
+router.put('/:id', function (req, res) {
+    const title = req?.body?.title;
+    const link = req?.body?.link;
+    const duration = req?.body?.duration;
+    const budget = req?.body?.budget;
+  
+    if (
+      !req.body ||
+      !title ||
+      !title.trim() ||
+      !link ||
+      !link.trim() ||
+      duration === undefined ||
+      typeof req?.body?.duration !== 'number' ||
+      duration < 0 ||
+      budget === undefined ||
+      typeof req?.body?.budget !== 'number' ||
+      budget < 0
+    )
+      return res.sendStatus(400);
+  
+    const id = req.params.id;
+    const indexOfMovieFound = MOVIES.findIndex((movie) => movie.id == id);
+  
+    if (indexOfMovieFound < 0) {
+      const newMovie = { id, title, link, duration, budget };
+      MOVIES.push(newMovie);
+      return res.json(newMovie);
+    }
+  
+    const moviePriorToChange = MOVIES[indexOfMovieFound];
+    const objectContainingPropertiesToBeUpdated = req.body;
+  
+    const updatedMovie = {
+      ...moviePriorToChange,
+      ...objectContainingPropertiesToBeUpdated,
+    };
+  
+    MOVIES[indexOfMovieFound] = updatedMovie;
+  
+    return res.json(updatedMovie);
+  });
+
+module.exports = router;
