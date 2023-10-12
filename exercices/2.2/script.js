b2bc576a72c40d69c97944d783703f33ff187799
dawid-ciborowski-vinci <dawid.ciborowski@student.vinci.be>
2023-10-12 17:00:17 +0200

    add: joke

--- exercices/2.2/script.js
+++ exercices/2.2/script.js
@@ -8,6 +8,7 @@ function counter() {
     h2.innerText = `Nombre de clics: ${count}`
     if (count == 5) p.textContent = "Bravo, bel échauffement !";
     else if (count == 10) p.textContent = "Vous êtes passé maître en l'art du clic !";
-}
+    else if (count == 69) window.open("https://youtu.be/dQw4w9WgXcQ?si=UqIvtPtb4kOYi1IL");
+};
 
 body.addEventListener('click', counter);
